792b6f7a2a9fe13d1e1909bb9c76f2e8dff7a181
mohamedhamdhy <mohamedalhamdhy@gmail.com>
2024-11-05 18:02:22 +0530

    Python Programming has been added

---
+++ Tutor_Python/02_ExerciseQuestionAndAnswers/03_TupleExercise/02_Exercise/Exercise.py
@@ -0,0 +1,9 @@
+# Write a Python program to multiply all the items in a list
+
+
+item=[3,4,5,4,7]
+total = 1
+for i in item:
+	total *= i
+print(total)
+ 
---
+++ Tutor_Python/02_ExerciseQuestionAndAnswers/08_MathExercise/01_Exercise/Exercise.py
@@ -0,0 +1,7 @@
+# Write a Python program to sum all the items
+
+item=[1,7,-10,34,2,-8]
+sum = 0
+for i in item:
+	sum+= i
+print(sum)
